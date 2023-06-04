5e094a7bcd36711d82013e724c55b645ce057c86
alghaffri <isami.abdullah@gmail.com>
2023-06-04 11:02:41 +0400

    Updating GetManagerRequestObject

--- src/main/java/com/codeline/sampleProject/RequestObjects/GetManagerRequestObject.java
+++ src/main/java/com/codeline/sampleProject/RequestObjects/GetManagerRequestObject.java
@@ -5,3 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class GetManagerRequestObject {
+    String department;
+
+    String teamName;
+}
