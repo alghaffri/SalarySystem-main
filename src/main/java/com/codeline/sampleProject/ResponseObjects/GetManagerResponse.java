b839860f098d28219603d272f431fdec55cd41c5
alghaffri <isami.abdullah@gmail.com>
2023-06-04 11:03:28 +0400

    Updating GetManagerResponse

--- src/main/java/com/codeline/sampleProject/ResponseObjects/GetManagerResponse.java
+++ src/main/java/com/codeline/sampleProject/ResponseObjects/GetManagerResponse.java
@@ -5,3 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class GetManagerResponse {
+    String department;
+
+    String teamName;
+}
